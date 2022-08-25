1594cfb2c76e06206cda3b7f05f91816c04acb85
Petya <petianaidenova@gmail.com>
2022-08-25 15:12:34 +0200

    add getById and edit

--- server/src/controllers/furnitureController.js
+++ server/src/controllers/furnitureController.js
@@ -7,7 +7,6 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
   const item = {
     make: req.body.make,
     model: req.body.model,
@@ -22,7 +21,44 @@ router.post("/", async (req, res) => {
     const result = await api.create(item);
     res.json(result);
   } catch (err) {
+    console.error(err);
     res.status(400).json({ message: "request error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const item = await api.getById(id);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).json({ message: `Item ${id} not found` });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const item = {
+    make: req.body.make,
+    model: req.body.model,
+    year: req.body.year,
+    description: req.body.description,
+    price: req.body.price,
+    img: req.body.img,
+    material: req.body.material,
+  };
+
+  try {
+    const result = await api.updateById(id, item);
+    res.json(result);
+  } catch (err) {
+    if (err._notFound) {
+      res.status(404).json({ message: `Item ${id} not found` });
+    } else {
+      console.error(err);
+      res.status(400).json({ message: "request error" });
+    }
   }
 });
 
--- server/src/services/furnitureService.js
+++ server/src/services/furnitureService.js
@@ -19,7 +19,35 @@ async function create(item) {
   return result;
 }
 
+async function getById(id) {
+  const item = await Item.findById(id);
+  return item;
+}
+
+async function updateById(id, item) {
+  const existing = await Item.findById(id);
+  if (existing) {
+    existing.make = item.make;
+    existing.model = item.model;
+    existing.year = item.year;
+    existing.description = item.description;
+    existing.price = item.price;
+    existing.img = item.img;
+    existing.material = item.material;
+
+    await existing.save();
+    return existing;
+  } else {
+    const error = new Error('Not Found');
+    error._notFound = true;
+    throw error;
+  }
+}
+
 module.exports = {
     getAll,
-    create
+    create,
+    getById,
+    updateById,
+
 }
