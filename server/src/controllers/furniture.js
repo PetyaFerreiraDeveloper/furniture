2d00716167d622481cb18b3fcaec89209b7b3bb6
Petya <petianaidenova@gmail.com>
2022-08-25 13:59:18 +0200

    add DB model for furniture item

--- server/src/controllers/furniture.js
+++ server/src/controllers/furniture.js
@@ -2,4 +2,12 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.json([]);
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    
+    res.end();
 })
+
+module.exports = router;
