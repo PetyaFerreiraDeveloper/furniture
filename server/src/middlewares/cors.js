9e59b6abccf3eb9a35c453426f2dc3b4859b05bb
Petya <petianaidenova@gmail.com>
2022-08-25 13:59:46 +0200

    add CORS Header for content type

--- server/src/middlewares/cors.js
+++ server/src/middlewares/cors.js
@@ -1,7 +1,7 @@
 module.exports = () => (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'get, post, put, delete, head, options');
-    // res.setHeader('Access-Control-Allow-Headers', '');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
 
     next();
