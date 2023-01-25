57a823e64baaed0c3129dbcbe0b7b82fe51458c1
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-01-25 10:59:54 +0100

    update

--- server/index 2.js
+++ server/index 2.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const cors = require('./src/middlewares/cors');
+const furnitureController = require('./src/controllers/furnitureController');
+const usersController = require('./src/controllers/usersController');
+const auth = require('./src/middlewares/auth');
 
 async function start() {
     try {
@@ -13,8 +16,14 @@ async function start() {
     }
 
     const app = express();
+
+    app.use(express.urlencoded({extended:true}));
     app.use(express.json());
     app.use(cors());
+    app.use(auth());
+
+    app.use('/data/catalog', furnitureController);
+    app.use('/users', usersController)
     
     app.listen(3030, () => {console.log('REST Service started on port 3030')});
 }
